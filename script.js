d5f4df2baf30900e54a8d3c02e09f5f362fe4997
Mark Hughes <wethestrongapparel@gmail.com>
2022-01-27 20:56:56 -0500

    Added twitter functionality and favicon

--- script.js
+++ script.js
@@ -3,11 +3,25 @@ const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author');
 const twitterBtn = document.getElementById('twitter');
 const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
 
 let apiQuotes = [];
 
+// Show Loading
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+// Hide Loader
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
 // Generate New Quote
 function newQuote() {
+  loading();
   // pick random quote
   const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
   // check if Author field is blank and replace with "Unknown".
@@ -24,11 +38,14 @@ function newQuote() {
   } else {
     quoteText.classList.remove('long-quote');
   }
+  // Set Quote, Hide Loader
   quoteText.textContent = quote.text;
+  complete();
 }
 
 // Get quotes from API
 async function getQuotes() {
+  loading();
   const apiUrl = 'https://type.fit/api/quotes';
   try {
     const response = await fetch(apiUrl);
@@ -38,5 +55,18 @@ async function getQuotes() {
 
   }
 }
+
+// Tweet Quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, '_blank');
+}
+
+// Event Listeners
+
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+
+
 // On Load
-getQuotes()
+getQuotes();
